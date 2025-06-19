99eddc313226fecef2600fdda0bbfc09a13c5ea3
angelzav <zavala_angel@hotmail.com>
2025-06-18 18:42:51 -0600

    challenge completed

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        ballImageView.alpha = 1
+        ballImageView.image = ballArray[Int.random(in: 0 ..< 5)]
+    }
+    
 
 }
 
